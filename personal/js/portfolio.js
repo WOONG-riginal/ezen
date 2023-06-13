dd89bfa9a558948add8b1effd7698c94afe2459c
WOONG-riginal <eggmoneyna26@gmail.com>
2023-06-13 22:43:45 +0900

    cm

---
+++ personal/js/portfolio.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container');
+const slider = document.querySelector('.slider');
+const s_wid = slider.offsetWidth;
+const s_li = slider.children;
+let win_wid = window.innerWidth;
+let slide_move_max = (s_wid - (win_wid/3)) * -1;
+let slide_move = 0;
+
+
+container.addEventListener('wheel',function(e){
+    move_slider(e.deltaY);
+    on_indicator();
+});
+
+function move_slider(amount){
+    slide_move -= amount;
+    if(slide_move < slide_move_max){
+        slide_move = slide_move_max;
+        return;
+    }else if(slide_move > 0){
+        slide_move = 0;
+        return;
+    }
+    slider.style.transform = `translateX(${slide_move}px)`;
+}
