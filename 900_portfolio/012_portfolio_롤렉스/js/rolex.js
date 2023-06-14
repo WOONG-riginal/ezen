80fb369ca1f0e349aaae924cb5f343ddc7f566a3
WOONG-riginal <eggmoneyna26@gmail.com>
2023-06-15 00:16:27 +0900

    rolex

--- 900_portfolio/012_portfolio_롤렉스/js/rolex.js
+++ 900_portfolio/012_portfolio_롤렉스/js/rolex.js
@@ -1,11 +1,64 @@
-var video=$('#mont_video');
-    var btn=$('#mont_btn');
-    btn.click(function(){
-        if(video.get(0).paused){
-            video.get(0).play();
-            btn.html('<i class="fa fa-pause-circle-o" aria-hidden="true"></i>');
-        }else{
-            video.get(0).pause();
-            btn.html('<i class="fa fa-play-circle-o" aria-hidden="true"></i>');
+/* 비디오 재생 스크립트 */
+const video = document.getElementById('mont_video');
+const videoBtn = document.getElementById('mont_btn');
+videoBtn.addEventListener('click', function(){
+    if(video.paused){
+        video.play();
+        btn.html('<i class="fa fa-pause-circle-o" aria-hidden="true"></i>');
+    }else{
+        video.pause();
+        btn.html('<i class="fa fa-play-circle-o" aria-hidden="true"></i>');
+    }
+});
+
+
+/* LNB 메뉴 오픈 스크립트 */
+const hamBtn = document.querySelector('.menu_btn');
+const hams = document.querySelector('.ham');
+const hamTxt = document.querySelectorAll('.ham_txt');
+const lnbMenu = document.querySelector('.lnb_wrapper');
+hamBtn.addEventListener('click', function(){
+    lnbMenu.classList.toggle('active');
+    hams.classList.toggle('X-btn');
+    hamTxt[0].classList.toggle('show');
+    hamTxt[1].classList.toggle('show');
+});
+
+
+/* LNB 서브메뉴 스크립트 */
+const lnbList = document.querySelectorAll('.lnb li');
+const sub_menuList = document.querySelectorAll('.sub_menu');
+for(var i=0; i<lnbList.length; i++){
+    lnbList[i].addEventListener('click', function(){
+        for(var j=0; j<lnbList.length; j++){
+            lnbList[j].classList.remove('on');
+            sub_menuList[j].classList.remove('display');
         }
-    });
+        this.classList.add('on');
+        let idx=$(this).index();
+        sub_menuList[idx].classList.add('display');
+    })
+}
+
+/* 컬렉션 메뉴 스크립트 */
+let movelength = 0;
+const upBtn = document.querySelector('.up-btn');
+const downBtn = document.querySelector('.down-btn');
+downBtn.addEventListener('click', function(){
+    movelength += 300;
+    if(movelength>=900) {
+        movelength=900;
+        $('.down-btn').fadeOut();
+    }
+    $('.up-btn').fadeIn();
+    $('.collection .sub_box').css({transform:'translateY(-'+movelength+'px)'});
+});
+upBtn.addEventListener('click', function(){
+    movelength -= 300;
+    if(movelength<=0) {
+        movelength=0;
+        $('.up-btn').fadeOut();
+    }
+    $('.down-btn').fadeIn();
+    $('.collection .sub_box').css({transform:'translateY(-'+movelength+'px)'});
+});
