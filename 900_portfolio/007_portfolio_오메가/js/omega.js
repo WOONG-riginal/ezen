d03db3790247d639be245d4e7b69261b06b82906
WOONG-riginal <eggmoneyna26@gmail.com>
2023-06-19 01:03:39 +0900

    we

--- 900_portfolio/007_portfolio_오메가/js/omega.js
+++ 900_portfolio/007_portfolio_오메가/js/omega.js
@@ -1,30 +1,30 @@
 /* 로딩화면 스크립트 */
-// window.onload=function(){
-//     $('.container').hide();
-//     beBright();
-//     setTimeout(beDark, 4500);
-//     setTimeout(loaded, 5500);
-//     setTimeout(startPage, 5500);
-//     setTimeout(beBright, 6000);
+window.onload=function(){
+    $('.container').hide();
+    beBright();
+    setTimeout(beDark, 4500);
+    setTimeout(loaded, 5500);
+    setTimeout(startPage, 5500);
+    setTimeout(beBright, 6000);
 
-//     setTimeout(addLine1, 1500);
-//     setTimeout(removeLine1, 2500);
-//     setTimeout(addLine2, 2600);
-//     setTimeout(removeLine2, 3600);
-//     setTimeout(addLine3, 3800);
-//     setTimeout(removeLine3, 4800);
-// }
+    setTimeout(addLine1, 1500);
+    setTimeout(removeLine1, 2500);
+    setTimeout(addLine2, 2600);
+    setTimeout(removeLine2, 3600);
+    setTimeout(addLine3, 3800);
+    setTimeout(removeLine3, 4800);
+}
 
-// function beBright() { $('.curtain').fadeOut(1000); }
-// function beDark() { $('.curtain').fadeIn(1000); }
-// function addLine1() { $('.line p').eq(0).show(); }
-// function addLine2() { $('.line p').eq(1).show(); }
-// function addLine3() { $('.line p').eq(2).show(); }
-// function removeLine1() { $('.line p').eq(0).hide(); }
-// function removeLine2() { $('.line p').eq(1).hide(); }
-// function removeLine3() { $('.line p').eq(2).hide(); }
-// function loaded() { $('.loading').hide(); }
-// function startPage() { $('.container').show(); }
+function beBright() { $('.curtain').fadeOut(1000); }
+function beDark() { $('.curtain').fadeIn(1000); }
+function addLine1() { $('.line p').eq(0).show(); }
+function addLine2() { $('.line p').eq(1).show(); }
+function addLine3() { $('.line p').eq(2).show(); }
+function removeLine1() { $('.line p').eq(0).hide(); }
+function removeLine2() { $('.line p').eq(1).hide(); }
+function removeLine3() { $('.line p').eq(2).hide(); }
+function loaded() { $('.loading').hide(); }
+function startPage() { $('.container').show(); }
 
 
 /* a링크 방지 스크립트*/
