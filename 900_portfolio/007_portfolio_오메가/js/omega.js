42953950d079d0f07a3dc2632ca5b118e4665d06
WOONG-riginal <eggmoneyna26@gmail.com>
2023-06-19 01:02:16 +0900

    we

--- 900_portfolio/007_portfolio_오메가/js/omega.js
+++ 900_portfolio/007_portfolio_오메가/js/omega.js
@@ -25,3 +25,67 @@
 // function removeLine3() { $('.line p').eq(2).hide(); }
 // function loaded() { $('.loading').hide(); }
 // function startPage() { $('.container').show(); }
+
+
+/* a링크 방지 스크립트*/
+$('a').click(function(e){
+    e.preventDefault();
+});
+
+
+/* 경고창 스크립트 */
+$('.lm li:nth-child(3),.lm li:nth-child(5),.logo,.rm li,.ham_menu-2f li,.no-action li,.detail,.buynow,.fnb-sub li,.sns ul li,.legal-menu li').click(function(){
+    $('.alert-message').fadeIn();
+    $('.alert-curtain').fadeIn();
+    setTimeout(function(){
+        $('.alert-message').fadeOut();
+        $('.alert-curtain').fadeOut();
+    },1000)
+});
+
+
+/* TOP 버튼 스크립트 */
+$(window).on('scroll',function(){
+    var scrT=$(this).scrollTop();
+    var screenwidth=screen.width;
+    if(screenwidth>412){
+        if(scrT>=500){
+            $('.top-btn').fadeIn();
+        }else{
+            $('.top-btn').fadeOut();
+        }
+    }
+});
+$('.top-btn').click(function(){
+    window.scrollTo({top: 0, behavior: 'smooth'}); 
+});
+
+
+/* HAM 메뉴 스크립트 */
+$('.ham_menu').hide();
+$('.ham-btn').click(function(){
+    $('.ham_menu').toggle(300);
+    $('.ham-btn p').toggleClass('no-show');
+    $('.ham_menu').toggleClass('cuntain-on');
+    $('.ham').toggleClass('X-btn');
+
+    if($('.ham_menu').hasClass('cuntain-on')){
+        $('.ham_curtain').show();
+        $('body').css({overflow:'hidden'})
+    }else{
+        $('.ham_curtain').hide();
+        $('body').css({overflow:'visible'})
+    }
+})
+
+$('.on-off .active').next('.ham_menu-2f').show();
+$('.on-off>li').click(function(){
+    if($(this).hasClass('active')){
+        $('.on-off>li').removeClass('active');
+    }else{
+        $('.on-off>li').removeClass('active');
+        $(this).addClass('active');
+    }
+    $('.on-off .ham_menu-2f').slideUp();
+    $('.on-off .active').next('.ham_menu-2f').slideDown();
+})
