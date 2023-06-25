dc62102b704d45eb529bc96993c88a8f488305a1
WOONG-riginal <eggmoneyna26@gmail.com>
2023-06-25 17:33:11 +0900

    cz

--- 900_portfolio/031_RWDT_gshock/js/gshock.js
+++ 900_portfolio/031_RWDT_gshock/js/gshock.js
@@ -1,6 +1,12 @@
 /* a링크 방지 스크립트*/
-$('a, button').click(function(e){
+$('.gnb li:nth-child(1) a,.gnb li:nth-child(2) a,.gnb li:nth-child(3) a,.gnb li:nth-child(4) a,.search_icon,.promotion_list a,.work_list a,.blog_list a,.send_btn').click(function(e){
     e.preventDefault();
+    $('.alert-message').fadeIn();
+    $('.alert-curtain').fadeIn();
+    setTimeout(function(){
+        $('.alert-message').fadeOut();
+        $('.alert-curtain').fadeOut();
+    },1000)
 });
 
 /* 박스 슬라이드 설정 스크립트 */
